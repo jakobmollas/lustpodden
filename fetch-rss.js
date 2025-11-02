10e72b946657ee93057e8f0353fd8114d9012d28
jakob-hqv <166600332+jakob-hqv@users.noreply.github.com>
2025-11-02 11:50:42 +0100

    Cleanup

--- fetch-rss.js
+++ fetch-rss.js
@@ -4,7 +4,7 @@ import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 import 'dotenv/config';
 
-const FEED_URL = process.env.PODBEAN_FEED_URL || 'https://feed.podbean.com/lustpodden/feed.xml';
+const FEED_URL = process.env.PODBEAN_FEED_URL;
 const OUT = './src/episodes.json';
 
 const parser = new Parser();
@@ -78,33 +78,6 @@ async function fetchAndWrite() {
   console.log(`Wrote ${episodes.length} episodes to ${OUT}`);
 }
 
-// async function fetchAndWrite() {
-//     console.log('Fetching RSS:', FEED_URL);
-//     const feed = await parser.parseURL(FEED_URL);
-//     console.log('Feed title:', feed.title);
-
-//     const episodes = feed.items.map((item, idx) => ({
-//         // Add/modify to suit the needs
-//         title: item.title || '',
-//         link: item.link || '',
-//         pubDate: new Date(item.pubDate).toISOString().split('T')[0],
-//         description: normalizeText(item.content || ''),
-//         durationMinutes: Math.floor((item.itunes?.duration || 0) / 60)  // duration = seconds
-
-//         // Enable this if we need tags
-//         // tags: (item.categories || item.itunes?.keywords || '')
-//         // ? // split keywords if they exist as a comma list
-//         // (Array.isArray(item.categories) && item.categories.length
-//         // ? item.categories
-//         // : String(item.itunes?.keywords || '').split(',').map(s => s.trim()).filter(Boolean))
-//         // : []
-//     }));
-
-//     writeFileSync(OUT, JSON.stringify(episodes, null, 2));
-
-//     console.log(`Wrote ${episodes.length} episodes to ${OUT}`);
-// }
-
 fetchAndWrite().catch(err => {
     console.error(err);
     process.exit(1);
@@ -113,7 +86,7 @@ fetchAndWrite().catch(err => {
 function normalizeText(text) {
     if (!text) return '';
     return text
-        .replace(/\u00A0/g, ' ')             // non-breaking space → normal space
+        .replace(/\u00A0/g, ' ')            // non-breaking space → normal space
         .replace(/\r\n/g, '\n')             // normalize CRLF
         .replace(/\n+/g, '<br>')            // convert one or more newlines → single <br>
         .replace(/(<br>\s*){2,}/g, '<br>')  // collapse multiple <br> in a row → one <br>
