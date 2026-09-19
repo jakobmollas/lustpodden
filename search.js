a9ff875f6962fc20f39c0da36746dfd11ea6a4ff
jakobmollas <jakobmollas@users.noreply.github.com>
2026-09-19 01:20:09 +0000

    deploy: c2565c87346888c5140e4e4d587021c8eab1da71

---
+++ search.js
@@ -0,0 +1,69 @@
+(async function () {
+    async function loadEpisodes() {
+        const episodeFilename = "episodes.json"; 
+        const res = await fetch(episodeFilename);
+        if (!res.ok) 
+            throw new Error('Could not load ' + episodeFilename);
+
+        return res.json();
+    }
+
+    function render(list) {
+        const out = document.getElementById('results');
+        if (!out)
+            return;
+
+        if (!list || list.length === 0) {
+            out.innerHTML = '<div class="col-12">No results</div>';
+            return;
+        }
+
+        out.innerHTML = list.map(ep => `
+<div class="col-12 col-md-6 mb-3">
+    <div class="card h-100">
+        <div class="card-body">
+            <h5 class="card-title"><a href="${ep.link}">${ep.title}</a></h5>
+            <h6 class="card-subtitle mb-2 text-muted">${ep.pubDate || ''} (${ep.durationMinutes} minutes)</h6>
+            <a href="${ep.spotify}" class="card-link">${ep.spotify}</a>
+            <p class="card-text">${(ep.description || '')}</p>
+        </div>
+    </div>
+</div>
+`).join('');
+    }
+
+    try {
+        const episodes = await loadEpisodes();
+        render(episodes);
+
+        // Configure search parameters
+        const fuse = new Fuse(episodes, {
+            keys: [
+                { name: 'title', weight: 0.5 },
+                { name: 'description', weight: 0.3 },
+                { name: 'pubDate', weight: 0.2 }
+            ],
+            isCaseSensitive: false,       // Case-insensitive
+            shouldSort: false,            // Do not sort, use original order, which is descending episode order
+            includeScore: true,
+            threshold: 0.1,               // Adjust fuzziness (0 = exact only)
+            ignoreLocation: true,         // Ignore position in string
+            minMatchCharLength: 2,        // Ignore super-short matches
+            useExtendedSearch: true       // Enables operators like '="' (exact match)
+        });
+
+        const serachInput = document.getElementById('search-input');
+        serachInput.addEventListener('input', (e) => {
+            const query = e.target.value.trim();
+            if (!query)
+                return render(episodes);
+
+            const results = fuse.search(query).map(r => r.item);
+            render(results);
+        });
+    } catch (err) {
+        console.error(err);
+        const out = document.getElementById('results');
+        if (out) out.innerHTML = '<div class="col-12">Error loading episodes</div>';
+    }
+})();
